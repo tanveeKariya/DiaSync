da349963b6bb3395ff67159d5cc2558acc21b542
Tanvee Kariya <tanveekariya@gmail.com>
2025-06-09 11:13:50 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? './' : '/',
   server: {
-    https: false,
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        secure: false
+        secure: false,
       }
     }
   }
-})
+}));
